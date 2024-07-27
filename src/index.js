5cf1d45ceb0e3efe3bb7a7569a8ded9cda0352cf
stephenmwandau <stephen.mwandau@student.moringaschool.com>
2024-07-27 15:26:00 +0300

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+
+  let form = document.getElementById("create-task-form");
+    const taskList = document.getElementById("tasks");
+  
+    form.addEventListener("submit", (event) => {
+      event.preventDefault(); // Prevent default form submission
+  
+      let inputField = document.getElementById("new-task-description");
+      const task = inputField.value;
+  
+      const newTaskItem = document.createElement("li");
+      newTaskItem.textContent = task;
+      taskList.appendChild(newTaskItem);
+  
+      inputField.value = ""; 
+    });
 });
